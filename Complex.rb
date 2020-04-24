16cd0f3cefa1dc1e19413cfea9e221dd7f7e0bfb
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-04-25 00:25:19 +0200

    modify get_stats method

--- Complex.rb
+++ Complex.rb
@@ -1,9 +1,11 @@
 class ComplexNum 
 
-    @@plus = 0
-    @@multiply=0
-    @@bulk_add=0
-    @@bulk_multiply=0
+    # @@plus = 0
+    # @@multiply=0
+    # @@bulk_add=0
+    # @@bulk_multiply=0
+
+    @@stats = {:plus => 0, :multiply => 0, :bulk_add => 0, :bulk_multiply=>0}
 
     attr_accessor :real, :imaginary
     #assign real and imaginary when create new instance
@@ -14,20 +16,23 @@ class ComplexNum
 
     ##operator overloading
     def +(cn)
-        @@plus += 1
+        # @@plus += 1
+        @@stats[:plus] += 1
         addedComplexNum = ComplexNum.new(self.real+cn.real,self.imaginary+cn.imaginary)
         return addedComplexNum
     end
 
     def *(cn)
-        @@multiply += 1
+        # @@multiply += 1
+        @@stats[:multiply] += 1
         multipliedComplexNum = ComplexNum.new((self.real * cn.real-self.imaginary*cn.imaginary) , (self.real*cn.imaginary + cn.real*self.imaginary)) 
         return multipliedComplexNum
     end
 
     ##class methods as its usage not limited to instance
     def self.bulk_add(cns)
-        @@bulk_add+=1
+        # @@bulk_add+=1
+        @@stats[:bulk_add] += 1
         bulkAddedCn = ComplexNum.new(0,0)
         cns.each do |cn| 
             bulkAddedCn = bulkAddedCn + cn
@@ -36,7 +41,8 @@ class ComplexNum
     end
 
     def self.bulk_multiply(cns)
-        @@bulk_multiply+=1
+        # @@bulk_multiply+=1
+        @@stats[:bulk_multiply] += 1
         bulkMultipliedCn = cns.first
         cns.drop(1).each do |cn| 
             bulkMultipliedCn =  bulkMultipliedCn * cn 
@@ -45,7 +51,8 @@ class ComplexNum
     end  
     
     def self.get_stats
-        puts ("+ :#{@@plus} - *: #{@@multiply} - bulk_add: #{@@bulk_add} - bulk_multiply: #{@@bulk_multiply}")
+        # puts ("+ :#{@@plus} - *: #{@@multiply} - bulk_add: #{@@bulk_add} - bulk_multiply: #{@@bulk_multiply}")
+        puts (@@stats)
     end
 
 end
